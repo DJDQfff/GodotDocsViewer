36a78a7998fc92233b6e758669e362f1a9069714
DJDQfff <10173363+DJDQfff@user.noreply.gitee.com>
2022-02-24 07:13:53 +0800

    存在一个小问题

---
+++ RstFileParser/RstParagraph.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+using MyStandard20Library;
+
+namespace RstFileParser
+{
+    public static class Factory_RstParagraph
+    {
+        public void
+    }
+
+    public class RstParagraph : Paragraph
+    {
+        public RstParagraph Paarent { set; get; }
+        public RstParagraph Child { set; get; }
+
+        public RstParagraph (Paragraph paragraph) : base(paragraph.Content)
+        {
+        }
+    }
+}
